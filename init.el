defc9ea53fa874cb80d3b159348212a3adef15dd
Daniel Pettersson <daniel.pettersson@hovding.se>
2021-08-12 14:43:36 +0200

    Move global binding so custom minor mode for evil mode intercepts

--- init.el
+++ init.el
@@ -25,7 +25,6 @@
   (require 'use-package))
 
 (require 'org)
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -38,13 +37,17 @@
  '(awesome-tray-mode-line-active-color "#0031a9")
  '(awesome-tray-mode-line-inactive-color "#d7d7d7")
  '(custom-safe-themes
-   '("2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" "18cd5a0173772cdaee5522b79c444acbc85f9a06055ec54bb91491173bc90aaa" "7ea491e912d419e6d4be9a339876293fff5c8d13f6e84e9f75388063b5f794d6" "e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" "8e439513e6954daa1e8c440cb67e2878497c77b18dadd030e09a1984b3314074" "387b487737860e18cbb92d83a42616a67c1edfd0664d521940e7fbf049c315ae" "816bacf37139d6204b761fea0d25f7f2f43b94affa14aa4598bce46157c160c2" "3e335d794ed3030fefd0dbd7ff2d3555e29481fe4bbb0106ea11c660d6001767" "2375374404692785c0ccbcaae3eca1f0bbcb7764c4f3f4f9d5bd0a4be7e8e0e0" "4780d7ce6e5491e2c1190082f7fe0f812707fc77455616ab6f8b38e796cbffa9" "801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2" "d4131a682c4436bb5a61103d9a850bf788cbf793f3fd8897de520d20583aeb58" "dbade2e946597b9cda3e61978b5fcc14fa3afa2d3c4391d477bdaeff8f5638c5" default))
- '(flymake-error-bitmap '(flymake-double-exclamation-mark modus-theme-fringe-red))
- '(flymake-note-bitmap '(exclamation-mark modus-theme-fringe-cyan))
- '(flymake-warning-bitmap '(exclamation-mark modus-theme-fringe-yellow))
- '(highlight-tail-colors '(("#aecf90" . 0) ("#c0efff" . 20)))
+   (quote
+    ("998975856274957564b0ab8f4219300bca12a0f553d41c1438bbca065f298a29" "2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" "18cd5a0173772cdaee5522b79c444acbc85f9a06055ec54bb91491173bc90aaa" "7ea491e912d419e6d4be9a339876293fff5c8d13f6e84e9f75388063b5f794d6" "e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" "8e439513e6954daa1e8c440cb67e2878497c77b18dadd030e09a1984b3314074" "387b487737860e18cbb92d83a42616a67c1edfd0664d521940e7fbf049c315ae" "816bacf37139d6204b761fea0d25f7f2f43b94affa14aa4598bce46157c160c2" "3e335d794ed3030fefd0dbd7ff2d3555e29481fe4bbb0106ea11c660d6001767" "2375374404692785c0ccbcaae3eca1f0bbcb7764c4f3f4f9d5bd0a4be7e8e0e0" "4780d7ce6e5491e2c1190082f7fe0f812707fc77455616ab6f8b38e796cbffa9" "801a567c87755fe65d0484cb2bded31a4c5bb24fd1fe0ed11e6c02254017acb2" "d4131a682c4436bb5a61103d9a850bf788cbf793f3fd8897de520d20583aeb58" "dbade2e946597b9cda3e61978b5fcc14fa3afa2d3c4391d477bdaeff8f5638c5" default)))
+ '(flymake-error-bitmap
+   (quote
+    (flymake-double-exclamation-mark modus-theme-fringe-red)))
+ '(flymake-note-bitmap (quote (exclamation-mark modus-theme-fringe-cyan)))
+ '(flymake-warning-bitmap (quote (exclamation-mark modus-theme-fringe-yellow)))
+ '(highlight-tail-colors (quote (("#aecf90" . 0) ("#c0efff" . 20))))
  '(hl-todo-keyword-faces
-   '(("HOLD" . "#70480f")
+   (quote
+    (("HOLD" . "#70480f")
      ("TODO" . "#721045")
      ("NEXT" . "#5317ac")
      ("THEM" . "#8f0075")
@@ -61,24 +64,27 @@
      ("FIXME" . "#a0132f")
      ("XXX+" . "#972500")
      ("REVIEW" . "#005a5f")
-     ("DEPRECATED" . "#201f55")))
- '(ibuffer-deletion-face 'modus-theme-mark-del)
- '(ibuffer-filter-group-name-face 'modus-theme-mark-symbol)
- '(ibuffer-marked-face 'modus-theme-mark-sel)
- '(ibuffer-title-face 'modus-theme-pseudo-header)
+     ("DEPRECATED" . "#201f55"))))
+ '(ibuffer-deletion-face (quote modus-theme-mark-del))
+ '(ibuffer-filter-group-name-face (quote modus-theme-mark-symbol))
+ '(ibuffer-marked-face (quote modus-theme-mark-sel))
+ '(ibuffer-title-face (quote modus-theme-pseudo-header))
  '(ivy-posframe-display-functions-alist
-   '((swiper . ivy-posframe-display-at-window-bottom-left)
-     (t . ivy-posframe-display)))
- '(ivy-posframe-style 'frame-center)
+   (quote
+    ((swiper . ivy-posframe-display-at-window-bottom-left)
+     (t . ivy-posframe-display))))
+ '(ivy-posframe-style (quote frame-center))
  '(ivy-rich-mode 1)
- '(org-agenda-files '("~/org/todo.org"))
+ '(org-agenda-files (quote ("~/org/todo.org")))
  '(package-selected-packages
-   '(ace dracula-theme mini-frame orgit better-jumper highlight-parentheses esup dump-jump modus-operandi-theme shrface eww-lnum axe forge orderless embark-consult marginalia golden-ratio ivy-hydra hydra avy git-link sr-speedbar lsp-ivy vterm acme-theme cider olivetti centered-cursor-mode centered-window minimal-theme tao-theme zenburn-theme yaml-mode use-package restclient pyenv-mode org-projectile org-bullets linum-relative ivy-rich ivy-prescient ivy-posframe go-mode flycheck fish-completion evil-magit evil-leader evil-escape evil-collection eshell-prompt-extras esh-autosuggest ein eglot diminish dashboard counsel-projectile company-shell company-prescient company-posframe company-lsp))
- '(setcdr '(t . ivy-format-function-default) t)
+   (quote
+    (elpy counsel-tramp pyvenv ace dracula-theme mini-frame orgit better-jumper highlight-parentheses esup dump-jump modus-operandi-theme shrface eww-lnum axe forge orderless embark-consult marginalia golden-ratio ivy-hydra hydra avy git-link sr-speedbar lsp-ivy vterm acme-theme cider olivetti centered-cursor-mode centered-window minimal-theme tao-theme zenburn-theme yaml-mode use-package restclient pyenv-mode org-projectile org-bullets linum-relative ivy-rich ivy-prescient ivy-posframe go-mode flycheck fish-completion evil-magit evil-leader evil-escape evil-collection eshell-prompt-extras esh-autosuggest ein eglot diminish dashboard counsel-projectile company-shell company-prescient company-posframe company-lsp)))
+ '(setcdr (quote (t . ivy-format-function-default)) t)
  '(vc-annotate-background nil)
  '(vc-annotate-background-mode nil)
  '(vc-annotate-color-map
-   '((20 . "#a60000")
+   (quote
+    ((20 . "#a60000")
      (40 . "#721045")
      (60 . "#8f0075")
      (80 . "#972500")
@@ -95,10 +101,10 @@
      (300 . "#0031a9")
      (320 . "#2544bb")
      (340 . "#0000c0")
-     (360 . "#5317ac")))
+     (360 . "#5317ac"))))
  '(vc-annotate-very-old-color nil)
- '(warning-suppress-log-types '((use-package)))
- '(warning-suppress-types '((lsp-mode)))
+ '(warning-suppress-log-types (quote ((use-package))))
+ '(warning-suppress-types (quote ((lsp-mode))))
  '(xterm-color-names
    ["#000000" "#a60000" "#005e00" "#813e00" "#0031a9" "#721045" "#00538b" "#f0f0f0"])
  '(xterm-color-names-bright
@@ -115,3 +121,5 @@
  '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
  '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
 (put 'erase-buffer 'disabled nil)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
