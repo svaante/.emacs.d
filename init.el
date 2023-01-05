9be2b9c7556c5e650516b6c0ca05356118640351
Daniel Pettersson <daniel@dpettersson.net>
2023-01-05 22:43:02 +0100

    29 changes

--- init.el
+++ init.el
@@ -1,2 +1,21 @@
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((magit-todos-exclude-globs)
+     (diff-add-log-use-relative-names . t)
+     (vc-git-annotate-switches . "-w")))
+ '(send-mail-function 'smtpmail-send-it)
+ '(warning-suppress-types '((lsp-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.15))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.1)))))
